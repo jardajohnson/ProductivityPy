58689b0a25241b23bd011735c9aca066893db586
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-05-27 01:53:09 -0700

    add mongodb password hash logic

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, flash, session
+from bson.objectid import ObjectId
+
 from app import app, bcrypt
-from pprint import pprint
+
 from app.models.user import User
 
 
@@ -8,15 +10,21 @@ from app.models.user import User
 def dashboard():
     if not 'current_user' in session:
         return render_template('dashboard.html')
-    
-    return render_template('dashboard.html')
+
+    data = {
+        '_id': ObjectId(session['current_user'])
+    }
+    current_user = User.get_user(data)
+
+    return render_template('dashboard.html', user=current_user)
+
 
 @app.route('/user/new', methods=['POST'])
 def handle_new_user():
 
     if not User.validate_user(request.form):
         return redirect('/')
-    
+
     hash = bcrypt.generate_password_hash(request.form['password'])
 
     data = {
@@ -24,8 +32,8 @@ def handle_new_user():
         'email': request.form['email'],
         'password': hash
     }
-    
+
     current_user = User.save(data)
     session['current_user'] = current_user
-    
+
     return redirect('/')

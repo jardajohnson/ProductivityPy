eb7726a724e8a8a25f7552758ed786a129af1420
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-05-27 01:10:06 -0700

    add flash errors to template

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,7 +1,17 @@
 from flask import render_template, request, redirect, flash, session
 from app import app
+from pprint import pprint
+from app.models.user import User
 
 
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
+
+
+@app.route('/user/new', methods=['POST'])
+def handle_new_user():
+
+    if not User.validate_user(request.form):
+        return redirect('/')
+    return redirect('/')

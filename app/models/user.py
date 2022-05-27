6542de5d7c1536d6e02cf08d48b89f04d6087b28
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-05-27 00:17:25 -0700

    add user validation methods

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,9 @@
+from flask import flash
 from app.config.monfig import db
 from datetime import datetime as date
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
 
 class User:
     def __init__(self, data: dict) -> None:
@@ -8,3 +12,28 @@ class User:
         self.email = data['email']
         self.password = data['password']
         self.created_at = date.datetime.now()
+
+
+# * Static methods
+
+    @staticmethod
+    def validate_user(user) -> bool:
+        is_valid = True  # we assume this is true
+
+        if len(user['name']) < 2 or not user['name'].isalpha():
+            flash(
+                "First name must be at least 2 characters, and contain all alphabetic characters.", "register")
+            is_valid = False
+
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address", "register")
+            is_valid = False
+
+        if len(user['password']) < 4:
+            flash(
+                "Password must be at least 4 characters", "register")
+            is_valid = False
+        if user['confirm_password'] != user['password']:
+            flash("Passwords must match.", "register")
+            is_valid = False
+        return is_valid
